3367163a41a36651e3389023e17d750660ee855f
Hojun-pot <110947741+Hojun-pot@users.noreply.github.com>
2022-10-20 16:48:23 +0100

    me updated
    me updated 추가

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add Item'}
+                        {submitting ? '추가....' : '추가'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
